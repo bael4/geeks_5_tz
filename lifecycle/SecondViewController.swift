c5c8fc5bf07d1df2524bda346093de8ef59f37be
bael4 <bayel.ryspekov@gmail.com>
2023-03-19 15:03:02 +0600

    first commit

---
+++ lifecycle/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  lifecycle
+//
+//  Created by Баэль Рыспеков on 18/3/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+
+}
---
+++ lifecycle/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  lifecycle
+//
+//  Created by Баэль Рыспеков on 17/3/23.
+//
+
+
+
+
+import UIKit
+import SnapKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+
+}
+
